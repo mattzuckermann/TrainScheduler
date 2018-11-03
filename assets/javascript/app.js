c794d151716a20e8bf5d3951ff26fc89c5d3400f
Media Man <matt.zuckermann@my.wheaton.edu>
2018-11-02 17:05:05 -0700

    Updated moment.js so that it properly calculates the nextArrivalFormatted and minutesTillTrain times that are appended to the page.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,51 +25,51 @@ $("#submitButton").on("click", function () {
     event.preventDefault();
     var name = $("#inputName").val().trim();
     var destination = $("#inputDestination").val().trim();
-    var frequency = $("#inputFrequency").val().trim();
     var time = $("#inputTime").val().trim();
-    var minutesAway = "placeholder";
+    var frequency = $("#inputFrequency").val().trim();
 
     //=====================================================
-    //=====================================================
+
+    var timeFormat = "HH:mm";
+    var firstTimeConverter = moment(time, timeFormat);
     var currentTime = moment();
-    console.log("CURRENT TIME (UNIX) " + currentTime);
-    var convertedCurrentTime = currentTime.format("hh:mm");
-    console.log("CURRENT TIME (CONVERTED): " + convertedCurrentTime);
-    console.log(currentTime.format("dddd"));
-    console.log(currentTime.format("DDD"));
+    
+    var diffTime = currentTime.diff(moment(firstTimeConverter), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+    
+    var remainder = diffTime % frequency;
+    console.log("Remainder: " + remainder);
+    
+    var minutesTillTrain = frequency - remainder;
+    console.log("MINUTES TILL TRAIN: " + minutesTillTrain);
+    
+    var nextArrival = currentTime.add(minutesTillTrain, "minutes");
+    var nextArrivalFormatted = moment(nextArrival).format("HH:mm");
+    console.log(nextArrivalFormatted);
 
-    var laterTime = time;
-    var laterTimeFormat = "HH:mm";
-    var convertedTime = moment(laterTime, laterTimeFormat);
-    var firebaseConvertedTime = convertedTime.format("HH:mm A")
-    console.log(firebaseConvertedTime);
 
-    console.log("");
-    console.log(convertedTime.format("X"));
-    console.log(convertedTime.format("HH:mm"));
-    console.log(convertedTime.format("hh:mm a"));
-    console.log(convertedTime.format("hh:mm A"));
     //=====================================================
     //=====================================================
 
     if (name === "" || destination === "" || time === "" || frequency === "") {
         alert("You need to fill in all the form information!");
+        // $("#emptyErrorDiv").text("You need to fill in all the form information!");
     } else {
         //STORE DATA IN FIREBASE
         var trainEntry = database.ref().push({
             trainName: name,
             trainDestination: destination,
             trainFrequency: frequency,
-            trainNextArrival: firebaseConvertedTime,
-            trainMinutesAway: "placeholder"
+            trainNextArrival: nextArrivalFormatted,
+            trainMinutesAway: minutesTillTrain
         });
-
-        //RETURN TEXT FORM BOXES TO PLACEHOLDER TEXT
-        $("#inputName").val("");
-        $("#inputDestination").val("");
-        $("#inputTime").val("");
-        $("#inputFrequency").val("");
+        // $("#emptyErrorDiv").text("");
     }
+    //RETURN TEXT FORM BOXES TO PLACEHOLDER TEXT
+    $("#inputName").val("");
+    $("#inputDestination").val("");
+    $("#inputTime").val("");
+    $("#inputFrequency").val("");
 });
 
 database.ref().on("child_added", function (childSnapshot) {
@@ -79,14 +79,6 @@ database.ref().on("child_added", function (childSnapshot) {
     var trainFrequency = childSnapshot.val().trainFrequency;
     var trainNextArrival = childSnapshot.val().trainNextArrival;
     var trainMinutesAway = childSnapshot.val().trainMinutesAway;
-
-    // Log everything that's coming out of snapshot
-    console.log(trainName);
-    console.log(trainDestination);
-    console.log(trainFrequency);
-    console.log(trainNextArrival);
-    console.log(trainMinutesAway);
-    console.log("--------------");
 
     //ADD ROW TO TABLE 
     var tableBody = $("tbody");
@@ -106,35 +98,4 @@ database.ref().on("child_added", function (childSnapshot) {
 });
 
 
-
-
-
-// // Assumptions
-// var tFrequency = $("#inputFrequency").val().trim();
-
-// // Time is 3:30 AM
-// var firstTime = $("#inputTime").val().trim();
-
-// // First Time (pushed back 1 year to make sure it comes before current time)
-// var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
-// console.log(firstTimeConverted);
-
-// // Current Time
-// var currentTime = moment();
-// console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-
-// // Difference between the times
-// var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-// console.log("DIFFERENCE IN TIME: " + diffTime);
-
-// // Time apart (remainder)
-// var tRemainder = diffTime % tFrequency;
-// console.log(tRemainder);
-
-// // Minute Until Train
-// var tMinutesTillTrain = tFrequency - tRemainder;
-// console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-
-// // Next Train
-// var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-// console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+//The database must be constantly listening to the change in time as well as the change in time remaining until train arrives
